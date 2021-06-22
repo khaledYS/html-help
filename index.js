ceb853ce6a28640a3faea47bfd45ec6641466eec
khaledYS <73815164+khaledYS@users.noreply.github.com>
2021-06-23 00:07:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY BIRTHDAY!', ' Awash Al-Balshah' ],
+			strings: [ 'HAPPY BIRTHDAY!', ' Awash AlBalshah' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
